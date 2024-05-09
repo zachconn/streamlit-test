e0beabef69d1a179deba96be17a829bea17db401
zachconn <168455821+zachconn@users.noreply.github.com>
2024-05-09 11:00:06 -0400

    Update test-app.py

--- test-app.py
+++ test-app.py
@@ -14,22 +14,25 @@ with st.chat_message("user"):
  st.write("Hello!")
  st.line_chart(np.random.randn(100,2))
 
-st.chat_input("Say something!")
+def form_callback(list1):
+ with open('Experience-Reservation-Streamlit-Test.csv', 'a+') as f:
+  f.write(f"{list1[0], list1[1], list1[2], list1[3], list1[4], list1[5], list1[6], list1[7]}\n")
 
-with st.container():
-  st.chat_input("Say Something!")
+with st.form(key="my_form",clear_on_submit=True):
+    
+    st.write("Enter Note")
+    
+    child_first_name = st.text_input("Child's First Name")
+    child_last_name = st.text_input("Child's Last Name")
+    experience_type = st.selectbox("Which experience would you like to book today?", ("Star Making", "Star Viewing", "Rockin' Spa", "Olivia's Oasis"))
+    experience_date = st.date_input("Select a date!")
+    experience_time = st.time_input("Select a time!")
+    
+    submitted = st.form_submit_button("Submit")
 
-child_first_name = st.text_input("Child's First Name")
-child_last_name = st.text_input("Child's Last Name")
-experience_type = st.selectbox("Which experience would you like to book today?", ("Star Making", "Star Viewing", "Rockin' Spa", "Olivia's Oasis"))
-experience_date = st.date_input("Select a date!")
-experience_time = st.time_input("Select a time!")
-
-if st.button("Submit", type= "primary"):
- f = open("Experience-Reservation-Streamlit-Test.csv", 'w')
- writer = csv.writer(f, delimiter = ',')
- writer.writerow([experience_type, 691329, child_first_name, child_last_name, experience_date, experience_time, "", ""])
- f.close()
+    list1 = [experience_type, "", child_first_name, child_last_name, experience_date, experience_time, "", ""]
+    if submitted:
+        form_callback(list1)
  
 
 
