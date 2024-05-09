0ad5faa0449f9b206853dc3a3aa074565823d6e5
zachconn <168455821+zachconn@users.noreply.github.com>
2024-05-09 11:35:28 -0400

    Update test-app.py

--- test-app.py
+++ test-app.py
@@ -34,14 +34,11 @@ with st.form(key="my_form",clear_on_submit=True):
     list1 = [experience_type, "", child_first_name, child_last_name, experience_date, experience_time, "", ""]
     if submitted:
      st.write(list1)
-     newdf = pd.DataFrame(lsit1)
+     newdf = pd.DataFrame(list1)
      res.append(newdf)
      form_callback(list1)
 
 st.dataframe(res)
-st.download_button(label = "Download", 
-                  data = res, 
-                  file_name = "sttest.csv")
 
 
 tab1, tab2, tab3 = st.tabs(["Star Experience", "Rockin' Spa", "Olivia's Oasis"])
