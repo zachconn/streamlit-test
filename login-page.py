31faa92b5c4f7af064d00b1b85c7d6b81641f4a1
zachconn <168455821+zachconn@users.noreply.github.com>
2024-05-22 14:36:16 -0400

    Update login-page.py

--- login-page.py
+++ login-page.py
@@ -1,7 +1,3 @@
-import streamlit_authenticator as stauth
-
-name, authentication_status, username = authenticator.login('Login', 'main')
-
 with st.form(key="my_form"):
   username = st.text_input("Username")
   password = st.text_input("Password")
